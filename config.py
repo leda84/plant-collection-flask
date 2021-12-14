1f7d90475a3482993c640dc062c17e16338646f9
leda84 <laura.dejesus929@gmail.com>
2021-12-14 12:50:00 -0600

    added/started form to  add plant to database

--- config.py
+++ config.py
@@ -6,6 +6,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, 'env'))
 
 class Config():
+    """
+        Set Config variables for the flash app.
+        Using Environment variables where available
+        Create config variables if not done already
+    """
+
     FLASK_APP = os.environ.get('FLASK_APP')
     FLASK_ENV = os.environ.get('FLASK_ENV')
     SECRET_KEY = os.environ.get('SECRET_KEY') or "You will never guess!"
--- plant_collection/forms.py
+++ plant_collection/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms import validators
 from wtforms.validators import DataRequired, Email
 
 class UserLoginForm(FlaskForm):
@@ -12,4 +13,17 @@ class UserSignupForm(FlaskForm):
     last_name = StringField('Last Name:', validators=[DataRequired()])
     email = StringField('Email:', validators=[DataRequired(), Email()])
     password = PasswordField('Password:', validators=[DataRequired()])
+    submit_button = SubmitField()
+
+class PlantForm(FlaskForm):
+    name = StringField('Name:', validators=[DataRequired()])
+    room = StringField('Room in House:', validators=[DataRequired()])
+    plant_type = StringField('Type of Plant:', validators=[DataRequired()])
+    light = StringField('Light Needed:', validators=[DataRequired()])
+    description = StringField('Description:', validators=[DataRequired()])
+    water = StringField('Amount of Water Needed:', validators=[DataRequired()])
+    fertilizer = StringField('Fertilizer:', validators=[DataRequired()])
+    humidity = StringField('Humidity:', validators=[DataRequired()])
+    pests = StringField('Pest Control:', validators=[DataRequired()])
+    fun_fact = StringField('Fun Fact(s):', validators=[DataRequired()])
     submit_button = SubmitField()
